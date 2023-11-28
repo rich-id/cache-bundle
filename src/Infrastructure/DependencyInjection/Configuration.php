d5fbb21af9ded3c87514577d052430731c38acfb
Dumazeau <hdumazeau@richcongress.com>
2023-11-28 11:56:06 +0100

    Fix Configuration

--- src/Infrastructure/DependencyInjection/Configuration.php
+++ src/Infrastructure/DependencyInjection/Configuration.php
@@ -5,8 +5,13 @@ declare(strict_types=1);
 namespace RichId\CacheBundle\Infrastructure\DependencyInjection;
 
 use RichCongress\BundleToolbox\Configuration\AbstractConfiguration;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 
 class Configuration extends AbstractConfiguration
 {
     public const CONFIG_NODE = 'rich_id_cache';
+
+    protected function buildConfiguration(ArrayNodeDefinition $rootNode): void
+    {
+    }
 }
